d77066f8f305313ff01a695221c64d2e57a51372
Siddharth Sahu <siddharthsahu@Siddharths-MacBook-Pro.local>
2025-04-29 17:06:46 +0530

    feat: update k8s yamls and add helm charts

--- sampleTech/andromeda/frontend/service.js
+++ sampleTech/andromeda/frontend/service.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const http = require('http')
+
 const app = express()
 const port = 3000
 
@@ -6,8 +8,39 @@ app.get('/', (req, res) => {
   res.send('<p>My flashy website!</p>')
 })
 
-app.get('/ping', (req, res) => {
+app.get('/ping/', (req, res) => {
   res.send('pong')
+})
+
+app.get('/deep-ping/', (req, res) => {
+  const options = {
+    hostname: 'be-andromeda',
+    port: 80,
+    path: '/ping/',
+    method: 'GET'
+  }
+
+  const request = http.request(options, (response) => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+      data += chunk
+    })
+
+    response.on('end', () => {
+      if (data === 'pong') {
+        res.send('pong')
+      } else {
+        res.status(500).send('Internal Server Error')
+      }
+    })
+  })
+
+  request.on('error', (error) => {
+    res.status(500).send('Internal Server Error')
+  })
+
+  request.end()
 })
 
 app.listen(port, () => {
